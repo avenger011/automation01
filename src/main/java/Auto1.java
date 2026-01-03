dbef349b5aa961078c2499e572181fe93a857228
ganesh <saiganeshrao01@gmail.com>
2026-01-03 11:38:19 +0530

    testing acadamy automation

--- src/main/java/Auto1.java
+++ src/main/java/Auto1.java
@@ -10,7 +10,11 @@ public class Auto1 {
             driver.manage().window().maximize();
             driver.findElement(By.linkText("ALL COURSES")).click();
             driver.findElement(By.className("zen-course-thumbnail")).click();
-            driver.findElement(By.linkText("Sign In")).click();
+            driver.findElement(By.xpath("//*[@id=\"timeline-0-milestone-content-0\"]/ul/li[11]/span/a")).click();
+            String str=driver.findElement(By.xpath("//li[@id=\"sectionName\"]")).getText();
+            System.out.println(str);
+
+            //*[@id="player"]/div[5]/div[6]/div[1]/button
         }
 
     }
